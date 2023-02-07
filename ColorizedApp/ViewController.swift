ae955911a97edde2ba3bbce8e7eebf988af9204b
Liza Grossman <lisok.sam@gmail.com>
2023-02-07 22:02:02 +0200

    Ready

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colourView: UIView!
+    
+    @IBOutlet var redColourNumber: UILabel!
+    @IBOutlet var greenColourNumber: UILabel!
+    @IBOutlet var blueColourNumber: UILabel!
+    
+    @IBOutlet var redColourSlider: UISlider!
+    @IBOutlet var greenColourSlider: UISlider!
+    @IBOutlet var blueColourSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colourView.layer.cornerRadius = 20
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redColourSlider.value),
+            green: CGFloat(greenColourSlider.value),
+            blue: CGFloat(blueColourSlider.value),
+            alpha: 1
+        )
     }
-
-
+    
+    @IBAction func addRedColor() {
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redColourSlider.value),
+            green: CGFloat(greenColourSlider.value),
+            blue: CGFloat(blueColourSlider.value),
+            alpha: 1
+        )
+    }
+    @IBAction func addGreenColor() {
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redColourSlider.value),
+            green: CGFloat(greenColourSlider.value),
+            blue: CGFloat(blueColourSlider.value),
+            alpha: 1
+        )
+    }
+    @IBAction func addBlueColor() {
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redColourSlider.value),
+            green: CGFloat(greenColourSlider.value),
+            blue: CGFloat(blueColourSlider.value),
+            alpha: 1
+        )
+    }
 }
 
